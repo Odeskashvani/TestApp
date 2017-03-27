d80976758519fd1190816d7f7ffc581cbc090820
Odeskashvani <Odeskashvani@gmail.com>
2017-03-27 12:58:52 +0530

    morning changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,26 +11,20 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule, routingComponents } from './app.routing';
 import { HomeComponent } from './home/home.component';
-import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { APP_CONFIG, AppConfig } from './app.config';
-import { MyDatePickerModule } from 'mydatepicker';
-import { CalendarComponent } from "angular2-fullcalendar/src/calendar/calendar";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    HomeComponent,
-    CalendarComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule,
-    Ng2Bs3ModalModule,
-    MyDatePickerModule,
+    AppRoutingModule
   ],
   providers: [
     AuthenticationService,
